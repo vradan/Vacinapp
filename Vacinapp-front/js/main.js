d7749411fc6ee0eaae6ba9078e002fe04da86c74
Diego Moreira Alves <diego.moreira@inspira.com.br>
2015-11-05 19:17:27 -0200

    Criacao da tela de vacinas

--- Vacinapp-front/js/main.js
+++ Vacinapp-front/js/main.js
@@ -3,16 +3,23 @@ var main = (function () {
 	return {
 		init: function () {
 			main.openVacinnePlace();
+			main.AddVacinne();
 		},
 
 		openVacinnePlace : function(){
 			$("#wrapVacinnes input[type='checkbox']").on("click", function(){
 				var $state = $(this).is(":checked");
 				if($state){
-					$("#opa").modal();
+					$("#MyVacinnes").modal();
 				}else{
 					console.log("Deseja desmarcar essa vacina como aplicada?");
 				}
+			});
+		},
+
+		AddVacinne : function(){
+			$("#VacinneList a").on("click", function(){
+				$("#ContentAddVacinne").modal();
 			});
 		}
 
